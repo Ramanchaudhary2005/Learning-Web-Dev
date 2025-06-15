62326de33525098213687a0db0b78c72fcfc1c88
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-15 23:39:47 +0530

    modified:   Class15/array.js

--- Class15/array.js
+++ Class15/array.js
@@ -1,2 +1,35 @@
-const states = ["Delhi","Noida","Agra"]
-console.log(states)
+// const states = ["Delhi","Noida","Agra"]
+// console.log(states)
+
+// const student1 = {
+//     name: "Raman",
+//     city: "Khurja",
+//     bloodgroup: "B+"
+// }
+
+// const student2 = {
+//     name: "Vidhi",
+//     city: "Khurja",
+//     bloodgroup: "AB+"
+// }
+
+// const student3 = {
+//     name: "Ramlal",
+//     city: "Agra",
+//     bloodgroup: "B+"
+// }
+
+// const students = [student1, student2, student3]
+// console.log(students[0])
+
+//----------------------------------------------------------------------------------------------------
+
+// const fruits = Array(["Mango", "Banana", "Kiwi", "Grapes"])
+// console.log(fruits)
+// console.log(typeof(fruits))
+// console.log(Array.isArray(fruits))
+// // console.log(Array.isArray(student3))
+
+
+// -----------------------------------------------------------------------------------------------------
+
