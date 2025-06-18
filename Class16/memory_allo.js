60fd926faaec5126a446b6efabd0bbb3a9209c2a
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-18 09:57:17 +0530

    new file:   Class16/memory_allo.html 	new file:   Class16/memory_allo.js

---
+++ Class16/memory_allo.js
@@ -0,0 +1,55 @@
+// const car = {
+//     brand: "Tata",
+//     model: "Nexon",
+//     year: 2025,
+// }
+
+// // for(let i in car){
+// //     console.log('key=',i, 'value=', car[i])
+// // }
+
+// const keyvaluepair = Object.entries(car);
+// // console.log(keyvaluepair)
+
+// for(let i=0; i<keyvaluepair.length;i++){
+//     console.log(keyvaluepair[i][0], keyvaluepair[i][1]);
+// }
+
+
+// for(let i of keyvaluepair){
+//     // It will iterate on value
+//     console.log(i)
+// }
+
+//------------------------------------------------------------------------------------------------------------------------
+
+const laptops=[
+    {
+        os: "Windows",
+        ram: "16GB",
+        rom: "256GB"
+    },
+    {
+        os: "Mac",
+        ram: "16GB",
+        rom: "128GB"
+    }
+]
+
+console.log(laptops)
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+const  arjun={
+    name: "Arjun Kumar",
+    age: "20",
+    city: "Delhi",
+    hobbies: ["jhcjcgb","hgvchgsvcg","jhcbhs"],
+    marks: {
+        english: 56,
+        evs: 98,
+        physics: 99,
+    },
+}
+
+//MEMEORY ALLOCATION
