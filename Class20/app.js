25637870130a7cfc28be2d1f7779ff2376a54012
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-22 23:07:38 +0530

    modified:   Class20/app.js 	modified:   Class21/app.js

--- Class20/app.js
+++ Class20/app.js
@@ -1,19 +1,39 @@
 // ----PROMISES-----
-window.console.log("hello", new Date())
+// window.console.log("hello", new Date())
 
-const successCallback = (data) =>{
-    console.log("Success", new Date(), data);
-    const pr2 = data.json();
-    pr2.then((finalData) =>{
-        console.log(finalData)
-    })
-}
+// const successCallback = (data) =>{
+//     console.log("Success", new Date(), data);
+//     const pr2 = data.json();
+//     pr2.then((finalData) =>{
+//         console.log(finalData)
+//     })
+// }
 
-const failureCallback = (data) =>{
-    console.log("Erorr", new Date(), data);
-    alert("Network error")
-}
+// const failureCallback = (data) =>{
+//     console.log("Erorr", new Date(), data);
+//     alert("Network error")
+// }
 
-const pr = fetch('https://dummyjson.com/recipes')
-pr.then(successCallback).catch(failureCallback);
-console.log(pr);
+// const pr = fetch('https://dummyjson.com/recipes')
+// pr.then(successCallback).catch(failureCallback);
+// console.log(pr);
+console.log("Start")
+const pr = new Promise((res, rej)=>{
+    console.log("Promise resolving")
+    setTimeout(()=>{
+        res({
+        name: "Raman",
+        city: "Delhi",
+    });
+    }, 5000);
+});
+
+console.log("Mid");
+
+pr.then((data) =>{
+    console.log("Done", data);
+}).catch((err)=>{
+    console.log("error", err);
+})
+
+console.log("End")
--- Class21/app.js
+++ Class21/app.js
@@ -1,4 +1,32 @@
-const arr = ["Tata", "Mahindra", "hyundai"];
-// arr[3] = "honda";
-arr.push("Honda");
-console.log(arr);
+// const arr = ["Tata", "Mahindra", "hyundai"];
+// // arr[3] = "honda";
+// arr.push("Honda");
+// console.log(arr);
+
+const arr = {
+    name: "Raman",
+    age: 19,
+}
+
+const arr2 = {...arr}; // Now the name of arr2 changes only not both
+arr2.name = "Vidhi"
+
+// console.log(arr);
+// console.log(arr2)
+
+const arry = [
+    {
+        name: "Raman",
+        location: "Khurja"
+    },
+    {
+        name: "Vidhi",
+        location: "Khurja",
+    }
+]
+
+const myfun = (a) =>{
+    console.log(a);
+}
+
+const ans = arry.find(myfun);
