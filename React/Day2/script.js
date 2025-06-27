40509d90129f31c01b8783aa0db1ed36057ce6c5
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-27 23:54:27 +0530

    modified:   React/Day2/index.html 	modified:   React/Day2/script.js

--- React/Day2/script.js
+++ React/Day2/script.js
@@ -1,5 +1,5 @@
-const domroot = document.getElementById("root");
-const reactroot = ReactDOM.createRoot(domroot);
+// const domroot = document.getElementById("root");
+// const reactroot = ReactDOM.createRoot(domroot);
 
 
 
@@ -37,8 +37,20 @@ const reactroot = ReactDOM.createRoot(domroot);
 
 // ---------------------------------------------------------------------
 
-const heading = React.createElement('h1',{}, "title");
-const para = React.createElement('p',{},"Description1");
-const conatiner = React.createElement('div',{},[heading,para ]);
+// const heading = React.createElement('h1',{}, "title");
+// const para = React.createElement('p',{},"Description1");
+// const conatiner = React.createElement('div',{},[heading,para ]);
 
-reactroot.render(conatiner);
+// reactroot.render(conatiner);
+
+const Card = (
+    <div className = "card">
+        <h1>Title</h1>
+        <p>Description</p>
+    </div>
+);
+
+const domroot = document.getElementById("root");
+const reactroot = ReactDOM.createRoot(domroot);
+
+reactroot.render(Card);
