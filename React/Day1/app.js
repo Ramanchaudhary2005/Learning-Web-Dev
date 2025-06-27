ae6ea1e484a399597e1ac27e48cf471e9a280609
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-27 09:12:37 +0530

    modified:   React/Day1/app.js 	modified:   React/Day1/index.html

---
+++ React/Day1/app.js
@@ -0,0 +1,8 @@
+const domroot = document.getElementById("root")
+const newlistcontainer = document.createElement("ul");
+
+domroot.appendChild(newlistcontainer)
+
+const listitems = document.createElement('li')
+newlistcontainer.appendChild(listitems)
+listitems.innerText = "Item1"
