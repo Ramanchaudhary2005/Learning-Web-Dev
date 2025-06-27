8d6fd06f9dd467e06706279772c65426e8919cc5
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-27 11:22:02 +0530

    modified:   React/Day1/app.js 	modified:   React/Day1/index.html

--- React/Day1/app.js
+++ React/Day1/app.js
@@ -9,4 +9,13 @@
 
 
 // -------------------------------------------------------------------------------------
+const domroot = document.getElementById("root"); // real dom
+const reactroot = ReactDOM.createRoot(domroot); // virtual dom
 
+const item1 = React.createElement("li",{
+    className: "fancy-text"
+},"item-1");
+const item2 = React.createElement("li",{},"item-2");
+const listcontainer = React.createElement("ul",{}, item1, item2);
+
+reactroot.render(listcontainer);
