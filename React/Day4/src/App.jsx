ed96122b204b732033e4c124d1ce0cd86654d20a
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-30 23:10:30 +0530

    modified:   React/Day4/src/App.jsx

--- React/Day4/src/App.jsx
+++ React/Day4/src/App.jsx
@@ -1,13 +1,17 @@
-
-
+// State management = Manage data in component
+// creating stateful variables wity the help of react
 
 const App = ()=>{
+    let searchtext = "Hello"
     const handleSearch=(e)=>{
-        console.log(e.target.value);
+        const val = e.target.value;
+        searchtext = val;
+        console.log(searchtext);
     }
     return (
         <div>
             <input type="text" onKeyUp={handleSearch}/>
+            <h2>{searchtext}</h2>
         </div>
     )
 };
