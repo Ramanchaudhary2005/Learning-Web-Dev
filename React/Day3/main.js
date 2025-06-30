e29a672fa38065f33e725ca77ed78713f8dd257f
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-30 09:10:30 +0530

    new file:   .gitignore 	new file:   React/Day3/.parcel-cache/191a2b7ee8b7e3f9-RequestGraph 	new file:   React/Day3/.parcel-cache/582c43c847a96184-AssetGraph 	new file:   React/Day3/.parcel-cache/8db61e4597f73f0c-BundleGraph 	new file:   React/Day3/.parcel-cache/c8bd11c6dff2ae42-AssetGraph 	new file:   React/Day3/.parcel-cache/data.mdb 	new file:   React/Day3/.parcel-cache/lock.mdb 	new file:   React/Day3/.parcel-cache/snapshot-191a2b7ee8b7e3f9.txt 	modified:   React/Day3/index.html 	modified:   React/Day3/main.js 	modified:   React/Day3/node_modules/.package-lock.json 	modified:   React/Day3/package-lock.json 	modified:   React/Day3/package.json

--- React/Day3/main.js
+++ React/Day3/main.js
@@ -1,1 +1,14 @@
-import React from react
+import React from "./node_modules/react";
+import ReactDom from "react-dom/client";
+
+const domRoot = document.getElementById("root");
+const reactRoot = ReactDom.createRoot(domRoot);
+console.log(React);
+
+const App = ()=>{
+    return <div>
+        <h1>Hello from React App</h1>
+    </div>
+}
+
+reactRoot.render(App());
