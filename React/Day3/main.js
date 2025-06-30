17488c86ed2b975ed44bf6321de88acda0730641
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-06-30 09:20:25 +0530

    modified:   React/Day3/main.js

--- React/Day3/main.js
+++ React/Day3/main.js
@@ -5,9 +5,26 @@ const domRoot = document.getElementById("root");
 const reactRoot = ReactDom.createRoot(domRoot);
 console.log(React);
 
+const Header = () =>{
+    return<div>
+        <a>Home</a>
+        <a>Search</a>
+        <a>Help</a>
+    </div>
+}
+
+const Main = () =>{
+    return<main>
+        <section><h1>Title 1</h1><p>This is the dummy text</p></section>
+        <section><h1>Title 2</h1><p>This is the dummy text</p></section>
+        
+    </main>
+}
+
 const App = ()=>{
     return <div>
-        <h1>Hello from React App</h1>
+        <Header/>
+        <Main></Main>
     </div>
 }
 
