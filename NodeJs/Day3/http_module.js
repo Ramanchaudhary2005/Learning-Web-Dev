aa5700ae280865cfcf29a47f43e080bac349a0ce
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-07-12 17:22:29 +0530

    modified:   NodeJs/Day3/http_module.js

--- NodeJs/Day3/http_module.js
+++ NodeJs/Day3/http_module.js
@@ -2,7 +2,8 @@ const http = require("node:http");
 
 const server = http.createServer((req, res)=>{
     console.log("Hello");
-    res.end("Hello from nodejs");
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end("<h1>Hello from nodejs</h1>");
 });
 
 
