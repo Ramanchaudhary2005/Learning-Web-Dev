e1e188931131ad801546975fddc86509b60780c4
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-08-01 13:16:12 +0530

    modified:   NodeJs/Day4/app.js

--- NodeJs/Day4/app.js
+++ NodeJs/Day4/app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
     console.log("-->", req.method, req.url);
-    res.end("Done");
+
+    res.writeHead(200,{'content-type':'application/json'});
+
+    res.end(JSON.stringify({
+        isSuccess: true,
+        message: "Server is running fine!",
+    }));
 });
 
 server.listen(2900, () => {
