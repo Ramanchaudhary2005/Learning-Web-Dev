323ca8aa719e4fdcf33fc22efc6f4425ca4bab96
Ramanchaudhary2005 <ramanch7890@gmail.com>
2025-08-05 11:31:56 +0530

    modified:   NodeJs/Day4/external modules/Expressjs Api server/app.js

--- NodeJs/Day4/external modules/Expressjs Api server/app.js
+++ NodeJs/Day4/external modules/Expressjs Api server/app.js
@@ -1,3 +1,17 @@
 const express = require("express");
 
-const app = express()
+const app = express();
+
+app.get("/",(req, res)=>{
+    console.log("req res on '/'");
+    // res.end("hello");
+    res.json({
+        isSuccess: true,
+        message: "Server is running fine...",
+        data: {},
+    })
+})
+
+app.listen(2900,()=>{
+    console.log("--------server started--------");
+});
